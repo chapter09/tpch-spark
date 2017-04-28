56ffb4b3a4acb6075cc63fd132ae64b8ce6dc665
haowang <wh.sjtu@gmail.com>
2017-04-28 01:25:25 -0400

    update data uploading script

--- scripts/upload-to-hdfs.py
+++ scripts/upload-to-hdfs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import os
-import sys
+import os.path as path
 import subprocess
 from pyhocon import ConfigFactory
 
@@ -20,41 +20,41 @@ def run(cmd):
     return subprocess.run(cmd, shell=True, cwd=os.environ["HADOOP_HOME"])
 
 
-def put(f, dst, opts):
-    cmd = "./bin/hdfs dfs -put %s %s" % (f, dst)
+def put(f, dst, opts=None):
+    return run("./bin/hdfs dfs -put %s %s" % (f, dst))
 
 
 def mkdir(p):
-    cmd = "./bin/hdfs dfs -mkdir %s" % p
+    return run("./bin/hdfs dfs -mkdir -p %s" % p)
 
 
 def parse_config():
     return ConfigFactory.parse_file('../conf/application.conf')
 
 
-def raw_data_list(data_scale):
-    dbgen_path = os.path.abspath("../dbgen/")
-    return [os.path.join(dbgen_path, d + "-" + str(data_scale) + ".tbl") for d in RAW_DATA]
-
-
 def main():
-    if not os.path.exists(os.environ["HADOOP_HOME"]):
+    if not path.exists(os.environ["HADOOP_HOME"]):
         print("Please Specify HADOOP_HOME")
         exit(0)
 
     conf = parse_config()
-    procs = []
-    data_scale =  conf.get_int("all.data-scale")
+    data_scale = conf.get_int("all.data-scale")
     input_dir = conf.get_string("all.input-dir")
+    dbgen_path = path.abspath("../dbgen/")
 
-    data_file_list = raw_data_list(data_scale)
+    procs = []
+    for d in RAW_DATA:
+        procs.append(mkdir(input_dir + "/" + d + "-" + data_scale))
 
-    for f in data_file_list:
-        pass
+    [p.join() for p in procs]
+    procs = []
 
+    for d in RAW_DATA:
+        procs.append(put(path.join(
+            dbgen_path, d + "-" + str(data_scale) + ".tbl"),
+            input_dir + "/" + d + "-" + data_scale + ".txt"))
 
-
-
+    [p.join() for p in procs]
 
 
 if __name__ == '__main__':
