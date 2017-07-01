6068c4ad65faddacb08e73b3931cc85c62a9a3bd
Hao Wang <wh.sjtu@gmail.com>
2017-07-01 12:43:47 -0400

    add tc script for local use

--- scripts/tc.sh
+++ scripts/tc.sh
@@ -2,7 +2,7 @@
 
 dst_ip="10.12.3.40"
 dev="ens3"
-bw="100"
+bw="10"
 
 if [[ $(tc qdisc show dev ${dev} | grep 'htb') ]]; then
   sudo tc qdisc del dev ${dev} root handle 1: htb
@@ -17,5 +17,5 @@ fi
 
 tc qdisc add dev ens3 root handle 1: htb
 tc class add dev ens3 parent 1: classid 1:1 htb rate ${bw}mbit ceil ${bw}mbit
-tc filter add dev ens3 protocol ip parent 1: prio 1 u32 match ip src $dst_ip flowid 1:1
+tc filter add dev ens3 protocol ip parent 1: prio 1 u32 match ip dst $dst_ip flowid 1:1
 
