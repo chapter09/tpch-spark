791c9928c3e13cf5dd8e3dc827c24643d4b486cf
Hao Wang <haowang@ece.utoronto.ca>
2017-09-23 19:15:36 -0400

    update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,8 @@
 
-max=11
+max=22
 for i in `seq 1 $max`
 do
-  #spark-submit --class "main.scala.TpchQuery" target/scala-2.11/Spark-TPCH-queries-assembly-1.0.jar experiments/3/supplier-customer.conf
-  spark-submit --class "main.scala.TpchQuery" target/scala-2.11/Spark-TPCH-queries-assembly-1.0.jar experiments/3/supplier-nation.conf
-  sleep 10
+  echo "Running Query-$i"
+  spark-submit --class "main.scala.TpchQuery" ../target/scala-2.11/Spark-TPCH-queries-assembly-1.0.jar ../conf/application-q$i.conf
+  sleep 2
 done
